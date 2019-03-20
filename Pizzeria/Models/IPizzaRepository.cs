3e238cdf6bb10ec9e27f4b369edc2e62e98a0f4f
kalitawojciech <wojtek1311997@gmail.com>
2019-03-20 20:20:54 +0100

    added IPizzaRepository

---
+++ Pizzeria/Models/IPizzaRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizzeria.Models
+{
+    interface IPizzaRepository
+    {
+        IEnumerable<Pizza> GetPizzas();
+        Pizza GetPizzaById(int pizzaId);
+    }
+}
