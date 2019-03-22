e571e4e6d293d48655f7e61c39967795651135bf
kalitawojciech <wojtek1311997@gmail.com>
2019-03-22 23:09:38 +0100

    added HomeViewModel

---
+++ Pizzeria/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Pizzeria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizzeria.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pizza> Pizzas { get; set; }
+    }
+}
