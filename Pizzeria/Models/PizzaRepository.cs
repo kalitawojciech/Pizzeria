5c59e0303acd57df648299a85d4abade0e692fbd
kalitawojciech <wojtek1311997@gmail.com>
2019-03-24 13:46:30 +0100

    added using EF Core

---
+++ Pizzeria/Models/PizzaRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizzeria.Models
+{
+    public class PizzaRepository : IPizzaRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PizzaRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public Pizza GetPizzaById(int pizzaId)
+        {
+            return _appDbContext.Pizzas.FirstOrDefault(pizza => pizza.Id == pizzaId);
+        }
+
+        public IEnumerable<Pizza> GetPizzas()
+        {
+            return _appDbContext.Pizzas;
+        }
+    }
+}
